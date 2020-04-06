6f2c9bedc19c58b7ecf8be0ffefdf1f38f2042fa
NanaMiyakoshi <not9yrv@gmail.com>
2020-04-06 18:04:17 +0900

    コメント追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     #1人のユーザーに複数のマイクロポストがある
     has_many :microposts
+    #存在しているか確認する
     validates :name, presence: true
     validates :email, presence: true
 end
